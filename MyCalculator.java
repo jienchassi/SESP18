de431f0aba26081a76269a3dd8daacf5cf5c3df2
Chris Salzwedel <37150129+Geno-Frofino@users.noreply.github.com>
2018-04-09 16:28:33 -0500

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -2,20 +2,24 @@ package mycalculator;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 /**
- *
- * @author Christopher
+ * @Authors: Christopher Salzwedel, Makenna Smith, Jasmin Enchassi
+ * @Description: This code creates a calculator with a GUI that can perform basic, scientific, and few business calculator functions.
+ *               More details can be found within the User Manual packaged with the code.
  */
-public class MyCalculator extends JFrame implements ActionListener  
+public class MyCalculator extends JFrame implements ActionListener  //myCalculator class begins
 {
-    JPanel[] rows = new JPanel[7]; //row making array
-    JButton[] button = new JButton[28]; //button making array
-    String[] ButtonString = {"(", "+/-", "C",
+    Scanner sc = new Scanner(System.in); //gets user input for business functions
+    JPanel[] rows = new JPanel[8]; //row making array
+    JButton[] button = new JButton[32]; //button making array
+    String[] ButtonString = {"10^x", "+/-", "C",
                              "BE", "|x|", "x̅", "ROI", "SALE",
                              "log", "7", "8", "9", "÷",
                              "ln", "4", "5", "6", "x",
                              "x²", "1", "2", "3", "-",
-                             "√", ".", "0", "=", "+"
+                             "√", ".", "0", "=", "+",
+                             "sin", "cos", "tan", "π"
     }; //button naming array
     boolean[] functions = new boolean[4]; //boolean array for equations' functions.
     double[] temporary = {0, 0};    //temporary double for later equations
@@ -23,17 +27,17 @@ public class MyCalculator extends JFrame implements ActionListener
     Font font = new Font("Calibri", Font.BOLD, 14); //Font of calculator
     
     Dimension displayDimension = new Dimension(300, 40); //Creates the dimension size for the display
-    Dimension buttonSize = new Dimension(60, 40); //Creates the dimension size for the buttons
+    Dimension buttonSize = new Dimension(70, 40); //Creates the dimension size for the buttons
     
     
     MyCalculator()  //Constructor
     {
         super("MyCalculator");
         setDesign(); //Uses an integrated design for the GUI
-        setSize(380, 250); //Sets the size of the entire calculator to be 380x250
+        setSize(380, 350); //Sets the size of the entire calculator to be 380x250
         setResizable(false); //Makes the calculator unresizable.
         setDefaultCloseOperation(EXIT_ON_CLOSE); //Allows exiting of the Calculator
-        GridLayout grid = new GridLayout(7,5); //Creates a grid for the buttons to be on (7 rows, 5 buttons per row)
+        GridLayout grid = new GridLayout(8,5); //Creates a grid for the buttons to be on (7 rows, 5 buttons per row)
         setLayout(grid); 
         
         for(int i = 0; i < 4; i++) //An array of functions used.
@@ -42,13 +46,13 @@ public class MyCalculator extends JFrame implements ActionListener
         FlowLayout flow1 = new FlowLayout(FlowLayout.CENTER);
         FlowLayout flow2 = new FlowLayout(FlowLayout.CENTER, 1, 1);
         
-        for(int i = 0; i < 7; i++) 
+        for(int i = 0; i < 8; i++) 
             rows[i] = new JPanel();
         
-        for(int i = 1; i < 7; i++) //Aligns the buttons to the center
+        for(int i = 1; i < 8; i++) //Aligns the buttons to the center
             rows[i].setLayout(flow2);
         
-        for(int i = 0; i < 28; i++) //Sets button font, texts bassed on ButtonString array, and adds actions
+        for(int i = 0; i < 32; i++) //Sets button font, texts bassed on ButtonString array, and adds actions
         {
             button[i] = new JButton();
             button[i].setText(ButtonString[i]);
@@ -61,7 +65,7 @@ public class MyCalculator extends JFrame implements ActionListener
         screen.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); //Orients the display
         screen.setPreferredSize(displayDimension); //Sets the dimension size
         
-        for(int i = 0; i < 28; i++) //Sets button size
+        for(int i = 0; i < 32; i++) //Sets button size
         {
             button[i].setPreferredSize(buttonSize);
         }
@@ -93,74 +97,95 @@ public class MyCalculator extends JFrame implements ActionListener
             rows[6].add(button[i]);
         add(rows[6]);
         
+        for(int i = 28; i < 32; i++)
+        rows[7].add(button[i]);
+        add(rows[7]);
+        
         setVisible(true); //makes the executable visible
     }
     
     public void actionPerformed(ActionEvent e)//Sets buttons to do their actions
     {
+        if(e.getSource() == button[0])//Sets the display to 10 to the power of the number given with the 10^x button
+            tenPower();
+        if(e.getSource() == button[1])//Swaps the number from negative to positive or positive to negative
+            posAndNeg();
+        if(e.getSource() == button[2])//Sets the Clear button to clear
+            clear();
+        if(e.getSource() == button[3])//Brings up the BreakEvenVisual Jframe with the BE button
+            BreakEven();
+        if(e.getSource() == button[4])//Sets the |x| button to create an absolute value of the number
+            AbsoluteValue();
+        if(e.getSource() == button[5])//Brings up the MeanVisual Jframe with the x̅ button
+            Mean();
+        if(e.getSource() == button[6])//Brings up the ROIVisual Jframe wiith the ROI button
+            ROI();
+        if(e.getSource() == button[7])//Brings up the Discount Visual Jfframe with the SALE button
+            Discount();
+        if(e.getSource() == button[8])//Sets the log button to create a logarithm of the number 
+            Logarithm();
         if(e.getSource() == button[9]) //Sets 7 button to display 7
             screen.append("7");
         if(e.getSource() == button[10])//Sets 8 button to display 8
             screen.append("8");
         if(e.getSource() == button[11])//Sets 9 button to display 9
             screen.append("9");
-        if(e.getSource() == button[14])//Sets 4 button to display 4
-            screen.append("4");
-        if(e.getSource() == button[15])//Sets 5 button to display 5
-            screen.append("5");
-        if(e.getSource() == button[16])//Sets 6 button to display 6
-            screen.append("6");
-        if(e.getSource() == button[19])//Sets 1 button to display 1
-            screen.append("1");
-        if(e.getSource() == button[20])//Sets 2 button to display 2
-            screen.append("2");
-        if(e.getSource() == button[21])//Sets 3 button to display 3
-            screen.append("3");
-        if(e.getSource() == button[24])//Sets . button to display .
-            screen.append(".");
-        if(e.getSource() == button[25])//Sets 0 button to display 0
-            screen.append("0");
-        if(e.getSource() == button[2]) //Sets the Clear button to clear
-            clear();
-        if(e.getSource() == button[18])// Sets the x^2 button to square the number
-            Squared();
-        if(e.getSource() == button[23])//Sets the √ button to square root the number
-            getSquareRoot();
-        if(e.getSource() == button[8])//Sets the log button to create a logarithm of the number 
-            Logarithm();
-        if(e.getSource() == button[13])//Sets the ln button to create a natural logarithm of the number
-            NaturalLogarithm();
-        if(e.getSource() == button[4])//Sets the |x| button to create an absolute value of the number
-            AbsoluteValue();
-        if(e.getSource() == button[26])//Displays the result of an equation given
-            getResult();
-        if(e.getSource() == button[1])//Swaps the number from negative to positive or positive to negative
-            posAndNeg();
-        if(e.getSource() == button[27])//Sets the + button to add 2 values
-            {
-            temporary[0] = Double.parseDouble(screen.getText());
-            functions[0] = true;
-            screen.setText("");
-            }
-        if(e.getSource() == button[22])//Sets the - button to subtract 2 values
-            {
-            temporary[0] = Double.parseDouble(screen.getText());
-            functions[1] = true;
-            screen.setText("");
-            }
-        if(e.getSource() == button[17])//Sets the x button to multiply 2 values
-            {
-            temporary[0] = Double.parseDouble(screen.getText());
-            functions[2] = true;
-            screen.setText("");
-            }
         if(e.getSource() == button[12])//Sets the ÷ button to divide 2 values
             {
             temporary[0] = Double.parseDouble(screen.getText());
             functions[3] = true;
             screen.setText("");
             }
-        
+        if(e.getSource() == button[13])//Sets the ln button to create a natural logarithm of the number
+            NaturalLogarithm();
+        if(e.getSource() == button[14])//Sets 4 button to display 4
+            screen.append("4");
+        if(e.getSource() == button[15])//Sets 5 button to display 5
+            screen.append("5");
+        if(e.getSource() == button[16])//Sets 6 button to display 6
+            screen.append("6");
+        if(e.getSource() == button[17])//Sets the x button to multiply 2 values
+            {
+            temporary[0] = Double.parseDouble(screen.getText());
+            functions[2] = true;
+            screen.setText("");
+            }
+        if(e.getSource() == button[18])// Sets the x^2 button to square the number
+            Squared();
+        if(e.getSource() == button[19])//Sets 1 button to display 1
+            screen.append("1");
+        if(e.getSource() == button[20])//Sets 2 button to display 2
+            screen.append("2");
+        if(e.getSource() == button[21])//Sets 3 button to display 3
+            screen.append("3");
+        if(e.getSource() == button[22])//Sets the - button to subtract 2 values
+            {
+            temporary[0] = Double.parseDouble(screen.getText());
+            functions[1] = true;
+            screen.setText("");
+            }
+        if(e.getSource() == button[23])//Sets the √ button to square root the number
+            getSquareRoot();
+        if(e.getSource() == button[24])//Sets . button to display .
+            screen.append(".");
+        if(e.getSource() == button[25])//Sets 0 button to display 0
+            screen.append("0");
+        if(e.getSource() == button[26])//Displays the result of an equation given
+            getResult();
+        if(e.getSource() == button[27])//Sets the + button to add 2 values
+            {
+            temporary[0] = Double.parseDouble(screen.getText());
+            functions[0] = true;
+            screen.setText("");
+            }
+        if(e.getSource() == button[28])//Uses the sin function of the currently displayed value with the sin button
+            Sin();
+        if(e.getSource() == button[29])//Uses the sin function of the currently displayed value with the cos button
+            Cos();
+        if(e.getSource() == button[30])//Uses the sin function of the currently displayed value with the tan button
+            Tan();
+        if(e.getSource() == button[31])//Prints pi to the 15th decimal with the π button
+            Pi();
     }
     
     public final void setDesign()//Designs the calculator
@@ -218,6 +243,11 @@ public class MyCalculator extends JFrame implements ActionListener
             screen.setText(Double.toString(value));
         }
     }
+    public void tenPower()//10^x function
+    {
+        double value = Math.pow(10, Double.parseDouble(screen.getText()));
+        screen.setText(Double.toString(value));
+    }
     
     public void getSquareRoot() //Square root function
     {
@@ -249,7 +279,55 @@ public class MyCalculator extends JFrame implements ActionListener
         screen.setText(Double.toString(value));
     }
     
-    public static void main(String[] args) 
+    public void Sin()//Sin function
+    {
+        double value = Math.sin(Double.parseDouble(screen.getText()));
+        screen.setText(Double.toString(value));
+    }
+    
+    public void Cos()//Cos function
+    {
+        double value = Math.cos(Double.parseDouble(screen.getText()));
+        screen.setText(Double.toString(value));
+    }
+    
+    public void Tan()//Tan function
+    {
+        double value = Math.tan(Double.parseDouble(screen.getText()));
+        screen.setText(Double.toString(value));
+    }
+    
+    public void BreakEven() //Calls the BreakEven Jframe to create a BreakEven display.
+    {
+       BreakEvenVisual be = new BreakEvenVisual();//Runs BreakEvenVisual Jframe
+       be.setVisible(true);
+    }
+    
+    public void ROI() //Calls the ROI Jframe to create a ROI display.
+    {
+       ROIVisual roi = new ROIVisual();
+       roi.setVisible(true);
+    }
+    
+    public void Discount() //Calls the Discount Jframe to create a Discount display.
+    {
+        DiscountVisual dv = new DiscountVisual();
+        dv.setVisible(true);
+    }
+    
+    public void Mean() //Calls the Mean Jframe to create a Mean display.
+    {
+        MeanVisual mv = new MeanVisual();
+        mv.setVisible(true);
+    }
+    
+    public void Pi() //Recreates the value of pi to the 15th decimal place.
+    {
+        double value = Math.PI;
+        screen.setText(Double.toString(value));            
+    }
+    
+    public static void main(String[] args) //main
     {
         MyCalculator calc = new MyCalculator(); //runs Calculator class
     }//end main
